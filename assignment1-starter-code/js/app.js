0bcabcb426132bfc06fb3bb7c2b4c72392df9be9
Ravi Subramanian <ravi.subram@gmail.com>
2017-12-08 21:22:09 -0600

    done

---
+++ assignment1-starter-code/js/app.js
@@ -0,0 +1,37 @@
+(function () {
+'use strict';
+
+angular.module('LunchCheck', [])
+
+.controller('LunchCheckController', function($scope) {
+	$scope.lunchList = "";
+	$scope.lunchMessage = "";
+    $scope.lnumber = 0;
+
+	$scope.displayMessage = function () {
+		var comma = ',';
+		var listArray;
+		var lunchItems = 0;
+		listArray = $scope.lunchList.split(comma);
+		lunchItems = listArray.length;
+		$scope.lnumber = lunchItems;
+				
+		
+				if (lunchItems <1) {
+					$scope.lunchMessage = "Enter atleast one item please";
+				}
+				if (lunchItems>1 && lunchItems<4) {
+					$scope.lunchMessage = "Enjoy";
+				}
+				if (lunchItems>3) {
+					$scope.lunchMessage = "Eating too much";
+				}
+				
+				
+
+
+	};
+
+});
+
+}) ();
